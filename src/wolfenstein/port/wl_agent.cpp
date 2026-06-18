b353c69682b3125180b3244cef2d2d7450d7dd7d
TheDiscordian <43145244+TheDiscordian@users.noreply.github.com>
2026-06-18 00:53:45 -0400

    Revert "blake: draw the item/enemy icon in the LINC info area (bstone ^SH/^AN)"
    This reverts commit 40ba93c516440c3504322722f6dd71fdfa381c67.

--- src/wolfenstein/port/wl_agent.cpp
+++ src/wolfenstein/port/wl_agent.cpp
@@ -374,11 +374,7 @@ void player_t::TakeDamage (int points, AActor *attacker)
 	{
 		extern const char *Blake_AttackerInfoMsg(AActor *attacker);
 		if (const char *amsg = Blake_AttackerInfoMsg(attacker))
-		{
 			StatusBar->DisplayInfoMessage(amsg, 0x200, 300);
-			extern void Blake_SetInfoIcon(const ClassDef *cls);
-			Blake_SetInfoIcon(attacker->GetClass());	// enemy icon (bstone ^AN)
-		}
 	}
 
 	StatusBar->UpdateFace(points);
